373d11841a6e8e7bc7abe123e981c4720a198fd4
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-01-22 23:44:07 -0600

    alternative solution using the while loop and an index

--- chunk/index-sol-2.js
+++ chunk/index-sol-2.js
@@ -11,16 +11,13 @@
 
 function chunk(array, size) {
 const chunked = [];
-for(let element of array){
-//get last element of chunked
-const last = chunked[chunked.length -1];
-//if last element does not exist or its length equals size,push element in new subarrays
-if(!last || last.length === size){
-   chunked.push([element]);
-}else{
-   last.push(element);
-}
+let index = 0;
+
+while (index < array.length){
+    chunked.push(array.slice(index,index + size));
+    index += size;
 }
 return chunked;
 }
+
 module.exports = chunk;
